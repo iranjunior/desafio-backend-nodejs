49ed305e2988f840cf31ca93f69fdc96161b041d
Iran Junior <iranjunior94@gmail.com>
2019-09-25 07:32:42 -0300

    new test for when field by undefined

--- __tests__/Integrations/users.spec.js
+++ __tests__/Integrations/users.spec.js
@@ -45,6 +45,19 @@ describe("Testar Criação de Usuarios", () => {
     expect(response.status).toBe(400);
   });
 
+  it("Deve retornar 400 caso o email ser do tipo undefined", async () => {
+    user.email = undefined;
+    const response = await request(app)
+      .post("/signup")
+      .send({
+        name: user.name,
+        email: user.email,
+        password: user.password,
+        phones: user.phones
+      });
+    expect(response.status).toBe(400);
+  });
+
   it("Deve retornar 400 caso o nome enviado seja invalido com apenas um caracter", async () => {
     user.name = "t";
     const response = await request(app)
