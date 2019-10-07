ec2b97ac918a6f6036bee59ce6e6f610d6dfe309
“Iran <iranjunior94@gmail.com>
2019-10-07 18:27:17 -0300

    swagger dont working

--- src/app.js
+++ src/app.js
@@ -1,16 +1,23 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const swaggerUI = require('swagger-ui-express');
 const helmet = require('helmet');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const routes = require('./routes');
 const mongoose = require('./Config/database');
 
 const app = express();
 app.use(helmet());
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.json());
 
 mongoose.connect();
 
+app.use('/', swaggerUI.serve, swaggerUI.setup(fs.readFileSync(path.resolve(__dirname, '..', 'swagger.json'))));
 app.use(routes);
 
 module.exports = app;
