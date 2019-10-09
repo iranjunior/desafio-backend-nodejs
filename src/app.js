098adb4be16e6f489fa025f2a6d461f86aedec2b
“Iran <iranjunior94@gmail.com>
2019-10-09 11:39:56 -0300

    add doc with swagger

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const path = require('path');
-const fs = require('fs');
+// const path = require('path');
+// const fs = require('fs');
 const swaggerUI = require('swagger-ui-express');
 const helmet = require('helmet');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 const mongoose = require('./Config/database');
+const swaggerConfig = require('../swagger.json');
 
 const app = express();
 app.use(helmet());
@@ -17,7 +18,7 @@ app.use(express.json());
 
 mongoose.connect();
 
-app.use('/', swaggerUI.serve, swaggerUI.setup(fs.readFileSync(path.resolve(__dirname, '..', 'swagger.json'))));
+app.use('/swagger', swaggerUI.serve, swaggerUI.setup(swaggerConfig));
 app.use(routes);
 
 module.exports = app;
